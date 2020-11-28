3a66c34e55f37226f06239f15e478ff3325cb7b6
John Kluse <j.kluse@gmail.com>
2020-11-28 16:49:58 -0600

    Ch 3 Input-Output Exercises

---
+++ CH3_Input-Output/ex2.js
@@ -0,0 +1,8 @@
+let rlSync = require('readline-sync');
+
+let fName = rlSync.question('What is your first name? ');
+let lName = rlSync.question('What is your last name? ');
+
+
+
+console.log(`Hello, ${fName} ${lName}!`);
